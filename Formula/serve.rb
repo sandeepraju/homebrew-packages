4af2bab806c0c3d4990cd2313171a9a0a2fa7ca5
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-01-08 09:23:10 -0800

    copies the file from nvm

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -3,19 +3,14 @@ class Serve < Formula
   desc "A simple HTTP server for serving static files."
   homepage "https://github.com/sandeepraju/serve"
   url "https://github.com/sandeepraju/serve/releases/download/v0.0.1/serve.zip"
-  mirror "https://github.com/sandeepraju/serve/releases/download/v0.0.1/serve.zip"
   sha256 "a295bafea1eccb4001562a98e571714caebe8673ff1dd410061ac8f432386805"
+  head "https://github.com/sandeepraju/serve.git"
 
   bottle :unneeded
 
-  head do
-    url "https://github.com/sandeepraju/serve.git"
-  end
-
-  keg_only :provided_by_osx
-
   def install
-    prefix.install Dir["*"]
+    prefix.install "serve", "serve"
+    bash_completion.install "bash_completion" => "serve"
   end
 
   test do
