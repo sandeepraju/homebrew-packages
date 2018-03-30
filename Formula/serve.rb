e8ecdf6a9a97e4d21b2d04d6491ef4d6d3b6febb
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-30 15:40:44 -0700

    update serve package

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -2,20 +2,11 @@
 class Serve < Formula
   desc "A simple HTTP server for serving static files."
   homepage "https://github.com/sandeepraju/serve"
-  url "https://github.com/sandeepraju/serve/releases/download/v0.0.1/serve.zip"
-  sha256 "a295bafea1eccb4001562a98e571714caebe8673ff1dd410061ac8f432386805"
-  head "https://github.com/sandeepraju/serve.git"
-
-  bottle :unneeded
-
+  url "https://github.com/sandeepraju/serve/files/1864893/serve-macos_x64.zip"
+  version "0.0.2"
+  sha256 "a0ffb4b3b4938b745f5312fa685c28b4d08932fa8df4f3c1a48455e639f1ae5f"
+  
   def install
-    prefix.install "serve"
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/serve"
-  end
-
-  test do
-    system "#{bin}/serve", "--version"
-    system libexec/"serve", "--version"
+    bin.install "serve"
   end
 end
